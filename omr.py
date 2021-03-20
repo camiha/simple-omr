e2c7af455bc88c1e14ee027b8446aa8557e7c5f3
cyamy <thecyamy@gmail.com>
2021-03-20 11:35:48 +0900

    fix variable name

--- omr.py
+++ omr.py
@@ -3,148 +3,148 @@ import pandas as pd
 import cv2
 import sys
 
-markerThreshold = 0.7
-threshold_value = 120
-Questions = 50
+marker_threshold_value = 0.7
+bin_threshold_value = 120
+questions = 50
 
 # メイン処理
 def main():
 
-    columns1 = ['grade','class','number']
+    column_1 = ['grade','class','number']
 
-    for i in range(1,Questions+1):
-        columns1.append('Q{}'.format(i))
+    for i in range(1,questions+1):
+        column_1.append('Q{}'.format(i))
 
-    df1 = pd.DataFrame(columns = columns1)
+    df1 = pd.DataFrame(columns = column_1)
 
     print("sheet count = ", end="")
-    sheetCount = int(input())
+    sheet_count = int(input())
 
     marker = cv2.imread('marker/marker.png', 0)
 
-    for sheetID in range(sheetCount):
+    for sheet_id in range(sheet_count):
         
-        studentDataOmrResult = []
-        answerArea1OmrResult = []
-        answerArea2OmrResult = []
+        omr_raw_student_data = []
+        omr_raw_answer_area1 = []
+        omr_raw_answer_area2 = []
 
         # グレースケールで外部画像データ読み取り
-        markSheet = cv2.imread("scandata/" + str(sheetID+1) + ".png", 0)
-        answerArea = identifyMarkArea(marker,markSheet)
+        mark_sheet = cv2.imread("scandata/" + str(sheet_id+1) + ".png", 0)
+        answer_area = identifyMarkArea(marker,mark_sheet)
 
-        # col,row,marginTop,marginBottom,img
-        resizedAnswerArea = omrSetUpResize(35,25,1,0,answerArea)
+        # col,row,margin_top,margin_bottom,img
+        resized_answer_area = omrSetUpResize(35,25,1,0,answer_area)
 
-        cv2.imwrite('out/resizedAnswerArea.png', resizedAnswerArea)
+        cv2.imwrite('out/resized_answer_area.png', resized_answer_area)
         
         ## 生徒情報処理
-        nameTag = resizedAnswerArea[700 : 1700, 190 : 690]
+        name_tag = resized_answer_area[700 : 1700, 190 : 690]
 
-        nameTag = cv2.rotate(nameTag, cv2.ROTATE_90_COUNTERCLOCKWISE)
-        studentDataOmrResult = setUpStudentData(omr(10,5,nameTag))
+        name_tag = cv2.rotate(name_tag, cv2.ROTATE_90_COUNTERCLOCKWISE)
+        omr_raw_student_data = setUpStudentData(omr(10,5,name_tag))
 
         ## 成績情報処理１
-        answerArea1 = resizedAnswerArea[100 : 2600, 1050 : 2050]
-        answerArea1OmrResult = omr(10,25,answerArea1)
+        answer_area1 = resized_answer_area[100 : 2600, 1050 : 2050]
+        omr_raw_answer_area1 = omr(10,25,answer_area1)
 
         ## 成績情報処理２
-        answerArea2 = resizedAnswerArea[100 : 2600, 2415 : 3415]
-        answerArea2OmrResult = omr(10,25,answerArea2)
+        answer_area2 = resized_answer_area[100 : 2600, 2415 : 3415]
+        omr_raw_answer_area2 = omr(10,25,answer_area2)
 
-        answerOmrResult = answerArea1OmrResult + answerArea2OmrResult
+        omr_raw_answer = omr_raw_answer_area1 + omr_raw_answer_area2
 
-        answer = toWriteData(studentDataOmrResult,answerOmrResult)
+        answer = toWriteData(omr_raw_student_data,omr_raw_answer)
 
-        df2 = pd.DataFrame(data = answer,columns = columns1)
+        df2 = pd.DataFrame(data = answer,columns = column_1)
         df1 = df1.append(df2, ignore_index = True)
 
     df1.to_csv("result.csv")
 
-def identifyMarkArea(marker,markSheet):
+def identifyMarkArea(marker,mark_sheet):
 
-    markArea = {}
+    mark_area = {}
 
     # マーカー位置の特定
-    markerLocation = cv2.matchTemplate(markSheet, marker, cv2.TM_CCOEFF_NORMED)
+    marker_location = cv2.matchTemplate(mark_sheet, marker, cv2.TM_CCOEFF_NORMED)
     # 特定用閾値の設定
-    markerLocation = np.where( markerLocation >= markerThreshold )
+    marker_location = np.where( marker_location >= marker_threshold_value )
 
     # マーク位置の切り取り
-    markArea['topX'] = min(markerLocation[1])
-    markArea['topY'] = min(markerLocation[0])
-    markArea['botX'] = max(markerLocation[1])
-    markArea['botY'] = max(markerLocation[0])
+    mark_area['topX'] = min(marker_location[1])
+    mark_area['topY'] = min(marker_location[0])
+    mark_area['botX'] = max(marker_location[1])
+    mark_area['botY'] = max(marker_location[0])
 
-    answerArea = markSheet[markArea['topY']:markArea['botY'],markArea['topX']:markArea['botX']]
+    answer_area = mark_sheet[mark_area['topY']:mark_area['botY'],mark_area['topX']:mark_area['botX']]
 
-    return answerArea
+    return answer_area
 
-def omrSetUpResize(col,row,marginTop,marginBot,img):
-    rowTotal = row + marginTop + marginBot
-    resizedImg = cv2.resize(img, (col*100, rowTotal*100))
+def omrSetUpResize(col,row,margin_top,margin_bot,img):
+    row_total = row + margin_top + margin_bot
+    resized_img = cv2.resize(img, (col*100, row_total*100))
 
-    return resizedImg
+    return resized_img
 
 def omr(col,row,img):
-    omrResult = []
+    omr_raw = []
 
-    blurredImg = cv2.GaussianBlur(img,(25,25),0)
-    binarizationImg = blurredImg.copy()
+    blur_img = cv2.GaussianBlur(img,(25,25),0)
+    binarization_img = blur_img.copy()
 
-    binarizationImg[blurredImg < threshold_value] = 0
-    binarizationImg[blurredImg >= threshold_value] = 255
+    binarization_img[blur_img < bin_threshold_value] = 0
+    binarization_img[blur_img >= bin_threshold_value] = 255
 
-    reversedImg = 255 - binarizationImg
+    reversed_img = 255 - binarization_img
 
 
     for row in range(1, row+1):
-        currentImg = reversedImg [(row-1)*100:row*100,]
-        areaSum = []
+        current_img = reversed_img [(row-1)*100:row*100,]
+        area_sum = []
 
-        for currentCol in range(col):
-            areaSum.append(np.sum(currentImg[:,currentCol*100:(currentCol+1)*100]))
+        for current_col in range(col):
+            area_sum.append(np.sum(current_img[:,current_col*100:(current_col+1)*100]))
         
-        omrResult.append(areaSum > np.median(255*300))
+        omr_raw.append(area_sum > np.median(255*300))
 
-    return omrResult
+    return omr_raw
 
-def setUpStudentData(omrResult):
-    studentRawData = []
-    studentData = [0,0,0]
+def setUpStudentData(omr_raw):
+    raw_student_data = []
+    student_data = [0,0,0]
     
     # omr結果を変換
-    for x in range(len(omrResult)):
-        temp = np.where(omrResult[x]==True)[0]
-        studentRawData.append(int(temp))
+    for x in range(len(omr_raw)):
+        temp = np.where(omr_raw[x]==True)[0]
+        raw_student_data.append(int(temp))
 
-    studentRawData = studentRawData[::-1]
+    raw_student_data = raw_student_data[::-1]
     # grade
-    studentData[0] = int(studentRawData[0])
+    student_data[0] = int(raw_student_data[0])
     # classNumber
-    studentData[1] = int(str(studentRawData[1]) + str(studentRawData[2]))
+    student_data[1] = int(str(raw_student_data[1]) + str(raw_student_data[2]))
     # studentNumber
-    studentData[2] = int(str(studentRawData[3]) + str(studentRawData[4]))
+    student_data[2] = int(str(raw_student_data[3]) + str(raw_student_data[4]))
 
-    return studentData
+    return student_data
 
-def toWriteData(studentDataOmrResult,answerOmrResult):
-    convertData = []
+def toWriteData(omr_raw_student_data,omr_raw_answer):
+    write_data = []
 
     for i in range(3):
-        convertData.append(studentDataOmrResult[i])
+        write_data.append(omr_raw_student_data[i])
 
-    for x in range(len(answerOmrResult)):
-        temp = np.where(answerOmrResult[x]==True)[0]+1
+    for x in range(len(omr_raw_answer)):
+        temp = np.where(omr_raw_answer[x]==True)[0]+1
         if len(temp)>1:
-            convertData.append('overlap')
+            write_data.append('overlap')
         elif len(temp)==1:
-            convertData.append(int(temp))
+            write_data.append(int(temp))
         else:
-            convertData.append('no data')
+            write_data.append('no data')
 
-    convertData = [convertData]
+    write_data = [write_data]
 
-    return convertData
+    return write_data
 
 if __name__=='__main__':
     main()
